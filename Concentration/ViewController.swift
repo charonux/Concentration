b20e61af02d6e39a462f5e42d0d5859fbca80c9a
Remus Barth <remusbarth@gmail.com>
2018-02-09 20:07:41 +0200

    2. Add more cards to the game.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -35,12 +35,12 @@ class ViewController: UIViewController {
             }
         }
     }
-    var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
+    var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎","🌓"]
     var emoji = [Int:String]()
     
     func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emoji.count)))
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
