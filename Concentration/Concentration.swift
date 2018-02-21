344a38e7b396975a908177cd5367940e95bcec7f
Remus Barth <remusbarth@gmail.com>
2018-02-21 22:00:38 +0200

    my version of computed property

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -1,47 +1,57 @@
-//
-//  Concentration.swift
-//  Concentration
-//
-//  Created by Kay Remus Barth on 26/01/2018.
-//  Copyright © 2018 Kay Remus Barth. All rights reserved.
-//
 import Foundation
-//class have inheritance and they are reference types
+
 class Concentration {
     var gameScore = 0
     var flipCount = 0
     var scoreMismatchPenalty = Array(repeating: 0, count: 20)
-    //public property
-    var cards = [Card]()//Array have also a init with no arguments to create an epmty array
-    var indexOfOneAndOnlyFaceUpCard: Int?
-    //public API method, user can only interact by choosing a card
-    func chooseCard(at index: Int){ //choosing card by index
+    var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var possibleReturnIndex = 0
+            var countOfFaceUpCards = 0
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    possibleReturnIndex = index
+                    countOfFaceUpCards += 1
+                }
+            }
+                if countOfFaceUpCards == 1 {
+                    return possibleReturnIndex
+                } else {
+                    return nil
+                }
+            }
+        set {
+            for index in cards.indices {
+                if index == newValue {
+                    cards[index].isFaceUp = true
+                } else {
+                    cards[index].isFaceUp = false
+                }
+            }
+        }
+    }
+    func chooseCard(at index: Int) { //choosing card by index
         flipCount += 1
         if !cards[index].isMatched {
             scoreMismatchPenalty[index] += 1 //the card is known now
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                //check if cards match
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                     gameScore += 2
                     scoreMismatchPenalty[index] = 0
                     scoreMismatchPenalty[matchIndex] = 0
+                    cards[index].isFaceUp = true
                 }
                 for temporaryIndex in 0...scoreMismatchPenalty.count - 1 {
                     if scoreMismatchPenalty[temporaryIndex] == 2 {
-                       gameScore -= 1
-                       scoreMismatchPenalty[temporaryIndex] = 0
+                        gameScore -= 1
+                        scoreMismatchPenalty[temporaryIndex] = 0
                     }
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                //either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
